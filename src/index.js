4671f596e802a2ca88503baacd7b4690fc1da5b8
Gajus Kuizinas <gajus@gajus.com>
2017-11-12 22:14:51 +0000

    feat: parse timestamp as number

--- src/index.js
+++ src/index.js
@@ -51,8 +51,21 @@ export {
   NotFoundError
 };
 
-types.setTypeParser(20, (value) => {
+// eslint-disable-next-line id-match
+const INT8_OID = 20;
+const TIMESTAMPTZ_OID = 1184;
+const TIMESTAMP_OID = 1114;
+
+types.setTypeParser(INT8_OID, (value) => {
   return parseInt(value, 10);
+});
+
+types.setTypeParser(TIMESTAMPTZ_OID, (value) => {
+  return value === null ? value : Date.parse(value);
+});
+
+types.setTypeParser(TIMESTAMP_OID, (value) => {
+  return value === null ? value : Date.parse(value);
 });
 
 const log = Logger.child({
