28cd45dcbd3e3c042b867a9d79b6bc583765b619
Gajus Kuizinas <gajus@gajus.com>
2019-02-24 10:20:49 +0000

    test: use createPool helper

--- test/slonik/binders/bindPool/interceptors/beforePoolConnection.js
+++ test/slonik/binders/bindPool/interceptors/beforePoolConnection.js
@@ -2,52 +2,7 @@
 
 import test from 'ava';
 import sinon from 'sinon';
-import bindPool from '../../../../../src/binders/bindPool';
-import log from '../../../../helpers/Logger';
-
-const createConnection = () => {
-  return {
-    connection: {
-      slonik: {
-        connectionId: '1'
-      }
-    },
-    query: () => {},
-    release: () => {}
-  };
-};
-
-const createPool = (clientConfiguration = {}) => {
-  const connection = createConnection();
-
-  const internalPool = {
-    connect: () => {
-      return connection;
-    },
-    slonik: {
-      poolId: '1'
-    }
-  };
-
-  const connectSpy = sinon.spy(internalPool, 'connect');
-  const releaseSpy = sinon.spy(connection, 'release');
-
-  const pool = bindPool(
-    log,
-    internalPool,
-    {
-      interceptors: [],
-      typeParsers: [],
-      ...clientConfiguration
-    }
-  );
-
-  return {
-    ...pool,
-    connectSpy,
-    releaseSpy
-  };
-};
+import createPool from '../../../../helpers/createPool';
 
 test('`beforePoolConnection` is called before `connect`', async (t) => {
   const beforePoolConnection = sinon.stub();
