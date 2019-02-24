5928eb766c3b35827439f4d95c53c494c1ef7048
Gajus Kuizinas <gajus@gajus.com>
2019-02-24 16:03:21 +0000

    fix: remove unused dependencies

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-/* eslint-disable no-process-env */
-
-import boolean from 'boolean';
-
-export const SLONIK_LOG_STACK_TRACE = boolean(process.env.SLONIK_LOG_STACK_TRACE);
-export const SLONIK_LOG_VALUES = boolean(process.env.SLONIK_LOG_VALUES);
