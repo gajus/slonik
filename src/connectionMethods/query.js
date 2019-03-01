d1f48d680aff226db54f1e61b0eae296dcc16c57
Gajus Kuizinas <gajus@gajus.com>
2019-03-01 16:15:59 +0000

    refactor: abstract query execution routine

--- src/connectionMethods/query.js
+++ src/connectionMethods/query.js
@@ -1,150 +1,24 @@
 // @flow
 
-import serializeError from 'serialize-error';
 import {
-  getStackTrace
-} from 'get-stack-trace';
-import {
-  createQueryId
-} from '../utilities';
-import {
-  BackendTerminatedError,
-  CheckIntegrityConstraintViolationError,
-  ForeignKeyIntegrityConstraintViolationError,
-  NotNullIntegrityConstraintViolationError,
-  QueryCancelledError,
-  UnexpectedStateError,
-  UniqueIntegrityConstraintViolationError
-} from '../errors';
+  executeQuery
+} from '../routines';
 import type {
-  InternalQueryFunctionType,
-  QueryContextType
+  InternalQueryFunctionType
 } from '../types';
 
 const query: InternalQueryFunctionType<*> = async (connectionLogger, connection, clientConfiguration, rawSql, values, inheritedQueryId) => {
-  if (connection.connection.slonik.terminated) {
-    throw new UnexpectedStateError('Cannot use terminated connection.');
-  }
-
-  const queryInputTime = process.hrtime.bigint();
-
-  let stackTrace = null;
-
-  if (clientConfiguration.captureStackTrace) {
-    const callSites = await getStackTrace();
-
-    stackTrace = callSites
-      .map((callSite) => {
-        return {
-          columnNumber: callSite.columnNumber,
-          fileName: callSite.fileName,
-          lineNumber: callSite.lineNumber
-        };
-      });
-  }
-
-  const queryId = inheritedQueryId || createQueryId();
-
-  const log = connectionLogger.child({
-    queryId
-  });
-
-  const originalQuery = {
-    sql: rawSql,
-    values
-  };
-
-  let actualQuery = {
-    ...originalQuery
-  };
-
-  const executionContext: QueryContextType = {
-    connectionId: connection.connection.slonik.connectionId,
-    log,
-    originalQuery,
-    poolId: connection.connection.slonik.poolId,
-    queryId,
-    queryInputTime,
-    stackTrace,
-    transactionId: connection.connection.slonik.transactionId
-  };
-
-  for (const interceptor of clientConfiguration.interceptors) {
-    if (interceptor.transformQuery) {
-      actualQuery = await interceptor.transformQuery(executionContext, actualQuery);
+  return executeQuery(
+    connectionLogger,
+    connection,
+    clientConfiguration,
+    rawSql,
+    values,
+    inheritedQueryId,
+    (finalConnection, finsalSql, finalValues) => {
+      return finalConnection.query(finsalSql, finalValues);
     }
-  }
-
-  let result;
-
-  for (const interceptor of clientConfiguration.interceptors) {
-    if (interceptor.beforeQueryExecution) {
-      result = await interceptor.beforeQueryExecution(executionContext, actualQuery);
-
-      if (result) {
-        return result;
-      }
-    }
-  }
-
-  const notices = [];
-
-  const noticeListener = (notice) => {
-    notices.push(notice);
-  };
-
-  connection.on('notice', noticeListener);
-
-  try {
-    result = await connection.query(actualQuery.sql, actualQuery.values);
-  } catch (error) {
-    // 'Connection terminated' refers to node-postgres error.
-    // @see https://github.com/brianc/node-postgres/blob/eb076db5d47a29c19d3212feac26cd7b6d257a95/lib/client.js#L199
-    if (error.code === '57P01' || error.message === 'Connection terminated') {
-      connection.connection.slonik.terminated = true;
-
-      throw new BackendTerminatedError();
-    }
-
-    if (error.code === '57014') {
-      throw new QueryCancelledError();
-    }
-
-    log.error({
-      error: serializeError(error),
-      queryId
-    }, 'query produced an error');
-
-    if (error.code === '23502') {
-      throw new NotNullIntegrityConstraintViolationError(error.constraint);
-    }
-
-    if (error.code === '23503') {
-      throw new ForeignKeyIntegrityConstraintViolationError(error.constraint);
-    }
-
-    if (error.code === '23505') {
-      throw new UniqueIntegrityConstraintViolationError(error.constraint);
-    }
-
-    if (error.code === '23514') {
-      throw new CheckIntegrityConstraintViolationError(error.constraint);
-    }
-
-    throw error;
-  } finally {
-    connection.off('notice', noticeListener);
-  }
-
-  result.notices = notices;
-
-  for (const interceptor of clientConfiguration.interceptors) {
-    if (interceptor.afterQueryExecution) {
-      result = await interceptor.afterQueryExecution(executionContext, actualQuery, result);
-    }
-  }
-
-  return result;
+  );
 };
 
 export default query;
--- src/routines/index.js
+++ src/routines/index.js
@@ -1,3 +1,4 @@
 // @flow
 
+export {default as executeQuery} from './executeQuery';
 export {default as setupTypeParsers} from './setupTypeParsers';
