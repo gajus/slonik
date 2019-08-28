c1134bb070cd7f359ba341da8c5b1f00e558051c
Gajus Kuizinas <gajus@gajus.com>
2019-08-28 23:06:26 +0100

    fix: remove unneeded $FlowFixMe

--- src/factories/createSqlTokenSqlFragment.js
+++ src/factories/createSqlTokenSqlFragment.js
@@ -44,50 +44,34 @@ import type {
 
 export default (token: SqlTokenType, greatestParameterPosition: number): SqlFragmentType => {
   if (token.type === SqlToken) {
-    // @see https://github.com/gajus/slonik/issues/36 regarding FlowFixMe use.
-    // $FlowFixMe
     return createSqlSqlFragment(token, greatestParameterPosition);
   } else if (token.type === RawToken) {
-    // $FlowFixMe
     return createRawSqlFragment(token, greatestParameterPosition);
   } else if (token.type === RawListToken) {
-    // $FlowFixMe
     return createRawListSqlFragment(token, greatestParameterPosition);
   } else if (token.type === IdentifierToken) {
-    // $FlowFixMe
     return createIdentifierSqlFragment(token);
   } else if (token.type === IdentifierListToken) {
-    // $FlowFixMe
     return createIdentifierListSqlFragment(token);
   } else if (token.type === ArrayToken) {
-    // $FlowFixMe
     return createArraySqlFragment(token, greatestParameterPosition);
   } else if (token.type === ValueListToken) {
-    // $FlowFixMe
     return createValueListSqlFragment(token, greatestParameterPosition);
   } else if (token.type === TupleToken) {
-    // $FlowFixMe
     return createTupleSqlFragment(token, greatestParameterPosition);
   } else if (token.type === TupleListToken) {
-    // $FlowFixMe
     return createTupleListSqlFragment(token, greatestParameterPosition);
   } else if (token.type === UnnestToken) {
-    // $FlowFixMe
     return createUnnestSqlFragment(token, greatestParameterPosition);
   } else if (token.type === ComparisonPredicateToken) {
-    // $FlowFixMe
     return createComparisonPredicateSqlFragment(token, greatestParameterPosition);
   } else if (token.type === BooleanExpressionToken) {
-    // $FlowFixMe
     return createBooleanExpressionSqlFragment(token, greatestParameterPosition);
   } else if (token.type === AssignmentListToken) {
-    // $FlowFixMe
     return createAssignmentListSqlFragment(token, greatestParameterPosition);
   } else if (token.type === JsonToken) {
-    // $FlowFixMe
     return createJsonSqlFragment(token, greatestParameterPosition);
   } else if (token.type === BinaryToken) {
-    // $FlowFixMe
     return createBinarySqlFragment(token, greatestParameterPosition);
   }
 
