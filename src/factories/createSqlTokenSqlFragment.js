f3b061311b88c51283d4776d4b451c1bcc60f7bd
Gajus Kuizinas <gajus@gajus.com>
2019-08-28 23:09:05 +0100

    style: sort alphabetically

--- src/factories/createSqlTokenSqlFragment.js
+++ src/factories/createSqlTokenSqlFragment.js
@@ -43,36 +43,36 @@ import type {
 } from '../types';
 
 export default (token: SqlTokenType, greatestParameterPosition: number): SqlFragmentType => {
-  if (token.type === SqlToken) {
-    return createSqlSqlFragment(token, greatestParameterPosition);
-  } else if (token.type === RawToken) {
-    return createRawSqlFragment(token, greatestParameterPosition);
-  } else if (token.type === RawListToken) {
-    return createRawListSqlFragment(token, greatestParameterPosition);
-  } else if (token.type === IdentifierToken) {
-    return createIdentifierSqlFragment(token);
-  } else if (token.type === IdentifierListToken) {
-    return createIdentifierListSqlFragment(token);
-  } else if (token.type === ArrayToken) {
+  if (token.type === ArrayToken) {
     return createArraySqlFragment(token, greatestParameterPosition);
-  } else if (token.type === ValueListToken) {
-    return createValueListSqlFragment(token, greatestParameterPosition);
-  } else if (token.type === TupleToken) {
-    return createTupleSqlFragment(token, greatestParameterPosition);
-  } else if (token.type === TupleListToken) {
-    return createTupleListSqlFragment(token, greatestParameterPosition);
-  } else if (token.type === UnnestToken) {
-    return createUnnestSqlFragment(token, greatestParameterPosition);
-  } else if (token.type === ComparisonPredicateToken) {
-    return createComparisonPredicateSqlFragment(token, greatestParameterPosition);
-  } else if (token.type === BooleanExpressionToken) {
-    return createBooleanExpressionSqlFragment(token, greatestParameterPosition);
   } else if (token.type === AssignmentListToken) {
     return createAssignmentListSqlFragment(token, greatestParameterPosition);
-  } else if (token.type === JsonToken) {
-    return createJsonSqlFragment(token, greatestParameterPosition);
   } else if (token.type === BinaryToken) {
     return createBinarySqlFragment(token, greatestParameterPosition);
+  } else if (token.type === BooleanExpressionToken) {
+    return createBooleanExpressionSqlFragment(token, greatestParameterPosition);
+  } else if (token.type === ComparisonPredicateToken) {
+    return createComparisonPredicateSqlFragment(token, greatestParameterPosition);
+  } else if (token.type === IdentifierListToken) {
+    return createIdentifierListSqlFragment(token);
+  } else if (token.type === IdentifierToken) {
+    return createIdentifierSqlFragment(token);
+  } else if (token.type === JsonToken) {
+    return createJsonSqlFragment(token, greatestParameterPosition);
+  } else if (token.type === RawListToken) {
+    return createRawListSqlFragment(token, greatestParameterPosition);
+  } else if (token.type === RawToken) {
+    return createRawSqlFragment(token, greatestParameterPosition);
+  } else if (token.type === SqlToken) {
+    return createSqlSqlFragment(token, greatestParameterPosition);
+  } else if (token.type === TupleListToken) {
+    return createTupleListSqlFragment(token, greatestParameterPosition);
+  } else if (token.type === TupleToken) {
+    return createTupleSqlFragment(token, greatestParameterPosition);
+  } else if (token.type === UnnestToken) {
+    return createUnnestSqlFragment(token, greatestParameterPosition);
+  } else if (token.type === ValueListToken) {
+    return createValueListSqlFragment(token, greatestParameterPosition);
   }
 
   throw new UnexpectedStateError();
