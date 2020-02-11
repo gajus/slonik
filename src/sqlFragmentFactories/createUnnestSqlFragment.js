203f016a49dbbff621924d0b311b1bf6b023dc0e
Gajus Kuizinas <gajus@gajus.com>
2020-02-11 15:19:56 +0800

    feat: allow to unnest buffer data

--- src/sqlFragmentFactories/createUnnestSqlFragment.js
+++ src/sqlFragmentFactories/createUnnestSqlFragment.js
@@ -53,8 +53,6 @@ export default (token: UnnestSqlTokenType, greatestParameterPosition: number): S
 
     for (const tupleValue of tupleValues) {
       if (!Array.isArray(tupleValue) && !isPrimitiveValueExpression(tupleValue) && !Buffer.isBuffer(tupleValue)) {
-        console.log('tupleValue', tupleValue, JSON.stringify(tupleValue));
-
         throw new InvalidInputError('Invalid unnest tuple member type. Must be a primitive value expression.');
       }
 
