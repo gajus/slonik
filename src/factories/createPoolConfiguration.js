59c3476d0f51d000b144da588d296ce2128516cf
Gajus Kuizinas <gajus@gajus.com>
2020-02-13 15:56:10 +0800

    fix: do not pass disabled timeout config (fixes #145)

--- src/factories/createPoolConfiguration.js
+++ src/factories/createPoolConfiguration.js
@@ -1,5 +1,7 @@
 // @flow
 
+/* eslint-disable id-match */
+
 import {
   parse as parseConnectionString,
 } from 'pg-connection-string';
@@ -12,38 +14,32 @@ export default (connectionUri: string, clientConfiguration: ClientConfigurationT
   const poolConfiguration = parseConnectionString(connectionUri);
 
   poolConfiguration.connectionTimeoutMillis = clientConfiguration.connectionTimeout;
-  // eslint-disable-next-line id-match
   poolConfiguration.idle_in_transaction_session_timeout = clientConfiguration.idleInTransactionSessionTimeout;
   poolConfiguration.idleTimeoutMillis = clientConfiguration.idleTimeout;
   poolConfiguration.max = clientConfiguration.maximumPoolSize;
-  // eslint-disable-next-line id-match
   poolConfiguration.statement_timeout = clientConfiguration.statementTimeout;
 
   if (clientConfiguration.connectionTimeout === 'DISABLE_TIMEOUT') {
-    poolConfiguration.connectionTimeout = 0;
+    poolConfiguration.connection_timeout = undefined;
   } else if (clientConfiguration.connectionTimeout === 0) {
     log.warn('connectionTimeout=0 sets timeout to 0 milliseconds; use connectionTimeout=DISABLE_TIMEOUT to disable timeout');
 
-    poolConfiguration.connectionTimeout = 1;
+    poolConfiguration.connection_timeout = 1;
   }
 
   if (clientConfiguration.idleInTransactionSessionTimeout === 'DISABLE_TIMEOUT') {
-    // eslint-disable-next-line id-match
-    poolConfiguration.idle_in_transaction_session_timeout = 0;
+    poolConfiguration.idle_in_transaction_session_timeout = undefined;
   } else if (clientConfiguration.idleInTransactionSessionTimeout === 0) {
     log.warn('idleInTransactionSessionTimeout=0 sets timeout to 0 milliseconds; use idleInTransactionSessionTimeout=DISABLE_TIMEOUT to disable timeout');
 
-    // eslint-disable-next-line id-match
     poolConfiguration.idle_in_transaction_session_timeout = 1;
   }
 
   if (clientConfiguration.statementTimeout === 'DISABLE_TIMEOUT') {
-    // eslint-disable-next-line id-match
-    poolConfiguration.statement_timeout = 0;
+    poolConfiguration.statement_timeout = undefined;
   } else if (clientConfiguration.statementTimeout === 0) {
     log.warn('statementTimeout=0 sets timeout to 0 milliseconds; use statementTimeout=DISABLE_TIMEOUT to disable timeout');
 
-    // eslint-disable-next-line id-match
     poolConfiguration.statement_timeout = 1;
   }
 
