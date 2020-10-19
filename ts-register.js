ab1ae5df9d6be0091939a1bca7ffb5107607535e
mmkal <misha.kaletsky@gmail.com>
2020-10-18 23:30:06 -0400

    tell nyc to cover ts files

--- ts-register.js
+++ ts-register.js
@@ -1,3 +1,4 @@
+// ava/nyc can't be told to just require `@babel/register` directly, because that ignores typescript by default
 require('@babel/register')({
   extensions: [".es6", ".es", ".jsx", ".js", ".mjs", ".ts"],
 })
