57b104071902bd47ed9a4f02365b07c83d645f06
mmkal <misha.kaletsky@gmail.com>
2020-10-19 11:49:46 -0400

    bump babel plugin version to avoid patch

--- ts-register.js
+++ ts-register.js
@@ -1,4 +1,6 @@
+/* eslint-disable filenames/match-regex, import/unambiguous, import/no-commonjs */
 // ava/nyc can't be told to just require `@babel/register` directly, because that ignores typescript by default
 require('@babel/register')({
-  extensions: [".es6", ".es", ".jsx", ".js", ".mjs", ".ts"],
-})
+  cache: false,
+  extensions: ['.es6', '.es', '.jsx', '.js', '.mjs', '.ts'],
+});
