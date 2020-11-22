01c348d03777c5303266ba395a056ee41f01a93e
mmkal <misha.kaletsky@gmail.com>
2020-11-22 15:03:57 -0500

    feat: include pg message in errors

--- src/errors.ts
+++ src/errors.ts
@@ -3,7 +3,18 @@
 import ExtendableError from 'es6-error';
 
 export class SlonikError extends ExtendableError {
-  originalError!: Error
+}
+
+export class WrappedPGError extends SlonikError {
+  readonly message!: string;
+
+  readonly originalError: Error
+
+  constructor (originalError: Error, message: string) {
+    super(`${message}: ${originalError.message}`);
+
+    this.originalError = originalError;
+  }
 }
 
 export class InvalidConfigurationError extends SlonikError {}
@@ -14,51 +25,42 @@ export class UnexpectedStateError extends SlonikError {}
 
 export class ConnectionError extends SlonikError {}
 
-export class StatementCancelledError extends SlonikError {
-  constructor (error: Error) {
-    super();
-
-    this.originalError = error;
-    this.message = 'Statement has been cancelled.';
+export class StatementCancelledError extends WrappedPGError {
+  constructor (error: Error, message = 'Statement has been cancelled') {
+    super(error, message);
   }
 }
 
 export class StatementTimeoutError extends StatementCancelledError {
   constructor (error: Error) {
-    super(error);
-
-    this.message = 'Statement has been cancelled due to a statement_timeout.';
+    super(error, 'Statement has been cancelled due to a statement_timeout');
   }
 }
 
-export class BackendTerminatedError extends SlonikError {
+export class BackendTerminatedError extends WrappedPGError {
   constructor (error: Error) {
-    super();
-
-    this.originalError = error;
-    this.message = 'Backend has been terminated.';
+    super(error, 'Backend has been terminated');
   }
 }
 
 export class NotFoundError extends SlonikError {
   constructor () {
-    super('Resource not found.');
+    super('Resource not found');
   }
 }
 
 export class DataIntegrityError extends SlonikError {
   constructor () {
-    super('Query returns an unexpected result.');
+    super('Query returns an unexpected result');
   }
 }
 
-export class IntegrityConstraintViolationError extends SlonikError {
+export class IntegrityConstraintViolationError extends WrappedPGError {
   constraint: string;
 
-  constructor (error: Error, constraint: string) {
-    super();
+  constructor (error: Error, constraint: string, message = 'Query violates an integrity constraint') {
+    super(error, message);
 
-    this.originalError = error;
     this.constraint = constraint;
   }
 }
@@ -68,36 +70,24 @@ export class IntegrityConstraintViolationError extends SlonikError {
 
 export class NotNullIntegrityConstraintViolationError extends IntegrityConstraintViolationError {
   constructor (error: Error, constraint: string) {
-    super(error, constraint);
-
-    this.originalError = error;
-    this.message = 'Query violates a not NULL integrity constraint.';
+    super(error, constraint, 'Query violates a not NULL integrity constraint');
   }
 }
 
 export class ForeignKeyIntegrityConstraintViolationError extends IntegrityConstraintViolationError {
   constructor (error: Error, constraint: string) {
-    super(error, constraint);
-
-    this.originalError = error;
-    this.message = 'Query violates a foreign key integrity constraint.';
+    super(error, constraint, 'Query violates a foreign key integrity constraint');
   }
 }
 
 export class UniqueIntegrityConstraintViolationError extends IntegrityConstraintViolationError {
   constructor (error: Error, constraint: string) {
-    super(error, constraint);
-
-    this.originalError = error;
-    this.message = 'Query violates a unique integrity constraint.';
+    super(error, constraint, 'Query violates a unique integrity constraint');
   }
 }
 
 export class CheckIntegrityConstraintViolationError extends IntegrityConstraintViolationError {
   constructor (error: Error, constraint: string) {
-    super(error, constraint);
-
-    this.originalError = error;
-    this.message = 'Query violates a check integrity constraint.';
+    super(error, constraint, 'Query violates a check integrity constraint');
   }
 }
