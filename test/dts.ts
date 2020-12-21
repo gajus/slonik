f515f8ddb7c4ab3877d5f44c23ff14815b4d7051
mmkal <misha.kaletsky@gmail.com>
2020-12-21 17:00:24 +0000

    test: ensure client configuration input is partial of client configuration

--- test/dts.ts
+++ test/dts.ts
@@ -30,6 +30,8 @@ import {
   createTypeParserPreset,
   sql,
   QueryResultType,
+  ClientConfigurationType,
+  ClientConfigurationInputType,
 } from '../src';
 
 const VALUE = 'foo';
@@ -38,6 +40,9 @@ const connection = createPool('postgres://');
 
 const poolTypes = () => {
   const pool = createPool('postgres://localhost');
+
+  expectTypeOf<ClientConfigurationType>().toMatchTypeOf<ClientConfigurationInputType>();
+  expectTypeOf<Partial<ClientConfigurationType>>().toEqualTypeOf<ClientConfigurationInputType>();
 
   const promise = pool.connect(async (conn) => {
     const result = await conn.query(sql`SELECT 1`);
