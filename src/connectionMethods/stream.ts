e94cf4b70f594c128ade8e97f32c01a870e1a7b4
alxndrsn <alxndrsn>
2022-07-30 16:20:54 +0300

    Close underlying QueryStream on stream.destroy()

--- src/connectionMethods/stream.ts
+++ src/connectionMethods/stream.ts
@@ -1,4 +1,6 @@
-import type Stream from 'stream';
+import {
+  type Readable,
+} from 'stream';
 import through from 'through2';
 import {
   QueryStream,
@@ -21,7 +23,7 @@ export const stream: InternalStreamFunction = async (connectionLogger, connectio
     (finalConnection, finalSql, finalValues, executionContext, actualQuery) => {
       const query = new QueryStream(finalSql, finalValues, options);
 
-      const queryStream: Stream = finalConnection.query(query);
+      const queryStream: Readable = finalConnection.query(query);
 
       const rowTransformers: Array<NonNullable<Interceptor['transformRow']>> = [];
 
@@ -66,6 +68,10 @@ export const stream: InternalStreamFunction = async (connectionLogger, connectio
 
         // Invoked if stream is destroyed using transformedStream.destroy().
         transformedStream.on('close', () => {
+          if (!queryStream.destroyed) {
+            queryStream.destroy();
+          }
+
           resolve({
             command: 'SELECT',
             fields: [],
@@ -73,6 +79,10 @@ export const stream: InternalStreamFunction = async (connectionLogger, connectio
             rowCount: 0,
             rows: [],
           });
+        });
+
+        transformedStream.on('error', (error: Error) => {
+          queryStream.destroy(error);
         });
 
         streamHandler(transformedStream);
--- test/slonik/integration/pg.ts
+++ test/slonik/integration/pg.ts
@@ -337,3 +337,39 @@ test('copies from binary stream', async (t) => {
 
   await pool.end();
 });
+
+test('frees connection after destroying a stream', async (t) => {
+  const pool = await createPool(t.context.dsn);
+
+  await pool.stream(sql`
+    SELECT * FROM GENERATE_SERIES(1, 100)
+  `, (stream) => {
+    stream.destroy();
+  });
+
+  t.deepEqual(await pool.anyFirst(sql`
+    SELECT TRUE
+  `), [
+    true,
+  ]);
+
+  await pool.end();
+});
+
+test('does not crash after destroying a stream with an error', async (t) => {
+  const pool = await createPool(t.context.dsn);
+
+  await pool.stream(sql`
+    SELECT * FROM GENERATE_SERIES(1, 100)
+  `, (stream) => {
+    stream.destroy(new Error('Foo'));
+  });
+
+  t.deepEqual(await pool.anyFirst(sql`
+    SELECT TRUE
+  `), [
+    true,
+  ]);
+
+  await pool.end();
+});
