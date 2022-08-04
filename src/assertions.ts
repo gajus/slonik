200aa0309a70aa31ce7ef6fa4239ab6b35ebdcc5
Gajus Kuizinas <gajus@gajus.com>
2022-08-04 15:09:16 -0500

    test: delete tests concerning json types

--- src/assertions.ts
+++ src/assertions.ts
@@ -2,7 +2,10 @@ import {
   type SqlSqlToken,
 } from './types';
 
-export const assertSqlSqlToken = (subject: SqlSqlToken | null): void => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnySqlSqlToken = SqlSqlToken<any>;
+
+export const assertSqlSqlToken = (subject: AnySqlSqlToken | null): void => {
   if (typeof subject !== 'object' || subject === null || subject.type !== 'SLONIK_TOKEN_SQL') {
     throw new TypeError('Query must be constructed using `sql` tagged template literal.');
   }
