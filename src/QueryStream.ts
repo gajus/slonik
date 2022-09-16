d322cf9f15f515495c60e28ac65a63d2bd46d9a7
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2022-09-16 23:30:51 +0300

    fix: close underlying QueryStream on stream.destroy() (#360)
    * Close underlying QueryStream on stream.destroy()
    
    * Update test name
    
    Co-authored-by: alxndrsn <alxndrsn>

--- src/QueryStream.ts
+++ src/QueryStream.ts
@@ -63,15 +63,10 @@ export class QueryStream extends Readable {
     this.cursor.submit(connection);
   }
 
-  public close (callback: Function) {
+  public _destroy (error: Error, callback: Function) {
     this._closed = true;
 
-    // eslint-disable-next-line unicorn/consistent-function-scoping
-    const close = () => {
-      this.emit('close');
-    };
-
-    this.cursor.close(callback || close);
+    this.cursor.close(callback);
   }
 
   public _read (size: number) {
