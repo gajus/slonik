4f7645f9b9b1330beffc39d40ef4050f85ab6b17
Gajus Kuizinas <gajus@gajus.com>
2023-09-26 12:29:15 -0400

    chore: remove unused utility sanitizeObject

--- src/utilities/sanitizeObject.ts
+++
@@ -1,7 +0,0 @@
-import { type SerializableValue } from '../types';
-
-export const sanitizeObject = (
-  object: Record<string, unknown>,
-): SerializableValue => {
-  return JSON.parse(JSON.stringify(object));
-};
