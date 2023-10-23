cd29a7cec6072e71f1eeb9c3014606f7f16a8f58
Gajus Kuizinas <gajus@gajus.com>
2023-10-23 10:55:56 -0600

    test: refactor TEST_ONLY

--- ava.config.js
+++ ava.config.js
@@ -1,22 +1,20 @@
 const {
-  TEST_ONLY_NON_INTEGRATION,
-  TEST_ONLY_PG_INTEGRATION,
-  TEST_ONLY_POSTGRES_INTEGRATION,
+  TEST_ONLY,
   // eslint-disable-next-line node/no-process-env
 } = process.env;
 
 module.exports = () => {
   let files = ['test/slonik/**/*'];
 
-  if (TEST_ONLY_NON_INTEGRATION) {
+  if (TEST_ONLY === 'utilities') {
     files = ['test/slonik/**/*', '!test/slonik/integration'];
   }
 
-  if (TEST_ONLY_PG_INTEGRATION) {
+  if (TEST_ONLY === 'pg-integration') {
     files = ['test/slonik/integration/pg.ts'];
   }
 
-  if (TEST_ONLY_POSTGRES_INTEGRATION) {
+  if (TEST_ONLY === 'postgres-integration') {
     files = ['test/slonik/integration/postgres.ts'];
   }
 
