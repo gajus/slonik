b9efe3b04f38911db3736b89cdc50aee04ada11a
Trond Marius Øvstetun <trond@ovstetun.no>
2024-03-12 04:59:47 +0100

    feat: Add field names in mockQueryResult (#534)
    Take all keys from first row, unspecified datatype (0). (fixes #232)
    
    Co-authored-by: Gajus Kuizinas <gajus@gajus.com>

--- src/factories/createMockQueryResult.ts
+++ src/factories/createMockQueryResult.ts
@@ -1,11 +1,16 @@
-import { type QueryResult, type QueryResultRow } from '../types';
+import { type Field, type QueryResult, type QueryResultRow } from '../types';
 
 export const createMockQueryResult = (
   rows: readonly QueryResultRow[],
 ): QueryResult<QueryResultRow> => {
+  let fields: Field[] = [];
+  if (rows.length > 0) {
+    fields = Object.keys(rows[0]).map((it) => ({ dataTypeId: 0, name: it }));
+  }
+
   return {
     command: 'SELECT',
-    fields: [],
+    fields,
     notices: [],
     rowCount: rows.length,
     rows,
