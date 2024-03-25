0388d18af27c3a040f11f0670cbc7fbc3a55a512
Gajus <gajus@gajus.com>
2024-03-25 10:35:55 -0600

    wip

--- src/utilities/formatSlonikPlaceholder.ts
+++ src/utilities/formatSlonikPlaceholder.ts
@@ -1,6 +1,8 @@
 /**
  * Slonik allows to compose queries using fragments, e.g.
+ * ```ts
  * sql.fragment`SELECT ${sql.fragment`${1}`}`;
+ * ```
  * Take a look at the logic in `createFragmentSqlFragment` to see how we detect nested fragments.
  * When we detect a nested fragment, we need to offset the index of the placeholder.
  * Historically, we used a placeholder format that was based on the index of the placeholder, e.g. $1, $2, etc.
