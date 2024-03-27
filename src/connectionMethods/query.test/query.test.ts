a9d2923303cb1b33081658abd9362358af65aaba
Gajus <gajus@gajus.com>
2024-03-27 11:40:28 -0600

    refactor: move error wrapping to pg driver

--- src/connectionMethods/query.test/query.test.ts
+++ src/connectionMethods/query.test/query.test.ts
@@ -1,15 +1,7 @@
-import {
-  BackendTerminatedError,
-  CheckIntegrityConstraintViolationError,
-  ForeignKeyIntegrityConstraintViolationError,
-  NotNullIntegrityConstraintViolationError,
-  UniqueIntegrityConstraintViolationError,
-} from '../../errors';
 import { createPgDriver } from '../../factories/createPgDriver';
 import { createPool } from '../../factories/createPool';
 import { createSqlTag } from '../../factories/createSqlTag';
 import { createErrorWithCode } from '../../helpers.test/createErrorWithCode';
-import { createPoolWithMockedQuery } from '../../helpers.test/createPoolWithMockedQuery';
 import { createTestRunner } from '../../helpers.test/createTestRunner';
 import * as sinon from 'sinon';
 
@@ -75,64 +67,4 @@ test('executes the query and returns the result', async (t) => {
     ],
     type: 'QueryResult',
   });
-});
-
-test('maps 23514 error code to CheckIntegrityConstraintViolationError', async (t) => {
-  const { pool, query } = await createPoolWithMockedQuery(t.context.dsn, {
-    driver,
-  });
-
-  query.rejects(createErrorWithCodeAndConstraint('23514'));
-
-  const error = await t.throwsAsync(pool.query(sql.unsafe`SELECT 1`));
-
-  t.true(error instanceof CheckIntegrityConstraintViolationError);
-});
-
-test('maps 23503 error code to ForeignKeyIntegrityConstraintViolationError', async (t) => {
-  const { pool, query } = await createPoolWithMockedQuery(t.context.dsn, {
-    driver,
-  });
-
-  query.rejects(createErrorWithCodeAndConstraint('23503'));
-
-  const error = await t.throwsAsync(pool.query(sql.unsafe`SELECT 1`));
-
-  t.true(error instanceof ForeignKeyIntegrityConstraintViolationError);
-});
-
-test('maps 23502 error code to NotNullIntegrityConstraintViolationError', async (t) => {
-  const { pool, query } = await createPoolWithMockedQuery(t.context.dsn, {
-    driver,
-  });
-
-  query.rejects(createErrorWithCodeAndConstraint('23502'));
-
-  const error = await t.throwsAsync(pool.query(sql.unsafe`SELECT 1`));
-
-  t.true(error instanceof NotNullIntegrityConstraintViolationError);
-});
-
-test('maps 23505 error code to UniqueIntegrityConstraintViolationError', async (t) => {
-  const { pool, query } = await createPoolWithMockedQuery(t.context.dsn, {
-    driver,
-  });
-
-  query.rejects(createErrorWithCodeAndConstraint('23505'));
-
-  const error = await t.throwsAsync(pool.query(sql.unsafe`SELECT 1`));
-
-  t.true(error instanceof UniqueIntegrityConstraintViolationError);
-});
-
-test('57P01 error causes the connection to be rejected (IMPLICIT_QUERY connection)', async (t) => {
-  const { pool, query } = await createPoolWithMockedQuery(t.context.dsn, {
-    driver,
-  });
-
-  query.rejects(createErrorWithCode('57P01'));
-
-  const error = await t.throwsAsync(pool.query(sql.unsafe`SELECT 1`));
-
-  t.true(error instanceof BackendTerminatedError);
 });
