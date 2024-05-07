8fb0cc0a7a19394d9cbd236810ec8ab24c48acc5
Gajus <gajus@gajus.com>
2024-05-06 18:41:03 -0700

    chore: add sql-tag-raw to monorepo

---
+++ packages/slonik-sql-tag-raw/src/utilities/interpolateNamedParameterReferences.test.ts
@@ -0,0 +1,80 @@
+import { interpolateNamedParameterReferences } from './interpolateNamedParameterReferences';
+import test from 'ava';
+import { sql } from 'slonik';
+
+test('does not error when placeholders are absent', (t) => {
+  const sqlFragment = interpolateNamedParameterReferences('SELECT 1', {});
+
+  t.is(sqlFragment.sql, 'SELECT 1');
+
+  t.deepEqual(sqlFragment.values, []);
+});
+
+test('interpolates a named parameter reference', (t) => {
+  const sqlFragment = interpolateNamedParameterReferences('SELECT :foo', {
+    foo: 'FOO',
+  });
+
+  t.is(sqlFragment.sql, 'SELECT $1');
+
+  t.deepEqual(sqlFragment.values, ['FOO']);
+});
+
+test('interpolates multiple named parameter references', (t) => {
+  const sqlFragment = interpolateNamedParameterReferences('SELECT :foo, :bar', {
+    bar: 'BAR',
+    foo: 'FOO',
+  });
+
+  t.is(sqlFragment.sql, 'SELECT $1, $2');
+
+  t.deepEqual(sqlFragment.values, ['FOO', 'BAR']);
+});
+
+test('interpolates multiple named parameter references (same name)', (t) => {
+  const sqlFragment = interpolateNamedParameterReferences('SELECT :foo, :foo', {
+    foo: 'FOO',
+  });
+
+  t.is(sqlFragment.sql, 'SELECT $1, $2');
+
+  t.deepEqual(sqlFragment.values, ['FOO', 'FOO']);
+});
+
+test('interpolates SQL token', (t) => {
+  const sqlFragment = interpolateNamedParameterReferences('SELECT :foo', {
+    foo: sql.fragment`${'foo'}`,
+  });
+
+  t.is(sqlFragment.sql, 'SELECT $1');
+
+  t.deepEqual(sqlFragment.values, ['foo']);
+});
+
+test('throws if named parameter references does not have a matching value', (t) => {
+  t.throws(
+    () => {
+      interpolateNamedParameterReferences('SELECT :foo, :bar', {
+        foo: 'FOO',
+      });
+    },
+    {
+      message: 'Named parameter reference does not have a matching value.',
+    },
+  );
+});
+
+test('throws if values object contains property names not present as named parameter references in the query', (t) => {
+  t.throws(
+    (): void => {
+      interpolateNamedParameterReferences('SELECT :foo', {
+        bar: 'BAR',
+        foo: 'FOO',
+      });
+    },
+    {
+      message:
+        'Values object contains value(s) not present as named parameter references in the query.',
+    },
+  );
+});
---
+++ packages/slonik-sql-tag-raw/src/utilities/interpolateNamedParameterReferences.ts
@@ -0,0 +1,65 @@
+import { Logger } from '../Logger';
+import { type NamedParameterValues } from '../types';
+import { interpolatePositionalParameterReferences } from './interpolatePositionalParameterReferences';
+import { type ValueExpression } from '@slonik/sql-tag';
+import { difference } from 'lodash';
+import { type FragmentSqlToken, InvalidInputError } from 'slonik';
+
+const log = Logger.child({
+  namespace: 'interpolateNamedParameterReferences',
+});
+
+/**
+ * @see https://regex101.com/r/KrEe8i/2
+ */
+const namedPlaceholderRegex = /[\s(,]:([_a-z]+)/gu;
+
+/**
+ * @see https://github.com/mysqljs/sqlstring/blob/f946198800a8d7f198fcf98d8bb80620595d01ec/lib/SqlString.js#L73
+ */
+export const interpolateNamedParameterReferences = (
+  inputSql: string,
+  inputValues: NamedParameterValues = {},
+): FragmentSqlToken => {
+  const resultValues: ValueExpression[] = [];
+  const parameterNames = Object.keys(inputValues);
+
+  for (const parameterName of parameterNames) {
+    const parameterValue = inputValues[parameterName];
+
+    resultValues.push(parameterValue);
+  }
+
+  const usedParameterNames = [] as string[];
+
+  const resultSql = inputSql.replaceAll(namedPlaceholderRegex, (match, g1) => {
+    if (!parameterNames.includes(g1)) {
+      throw new InvalidInputError(
+        'Named parameter reference does not have a matching value.',
+      );
+    }
+
+    usedParameterNames.push(g1);
+
+    const parameterIndex = parameterNames.indexOf(g1) + 1;
+
+    return match.slice(0, -g1.length - 1) + `$${parameterIndex}`;
+  });
+
+  const unusedParameterNames = difference(parameterNames, usedParameterNames);
+
+  if (unusedParameterNames.length > 0) {
+    log.warn(
+      {
+        unusedParameterNames,
+      },
+      'unused parameter names',
+    );
+
+    throw new InvalidInputError(
+      'Values object contains value(s) not present as named parameter references in the query.',
+    );
+  }
+
+  return interpolatePositionalParameterReferences(resultSql, resultValues);
+};
