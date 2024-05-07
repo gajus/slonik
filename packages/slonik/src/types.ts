e6991c92d8943d5a6db7fbbe58df95a7b944edce
Gajus <gajus@gajus.com>
2024-05-06 23:35:08 -0700

    style: remove unnecessary type member

--- packages/slonik/src/types.ts
+++ packages/slonik/src/types.ts
@@ -7,7 +7,6 @@ import { type SlonikError } from '@slonik/errors';
 import {
   type PrimitiveValueExpression,
   type QuerySqlToken,
-  type SqlFragmentToken,
   type SqlToken,
 } from '@slonik/sql-tag';
 import { type Readable } from 'node:stream';
@@ -292,10 +291,7 @@ export type QueryContext = {
   readonly transactionId: string | null;
 };
 
-export type ValueExpression =
-  | PrimitiveValueExpression
-  | SqlFragmentToken
-  | SqlToken;
+export type ValueExpression = PrimitiveValueExpression | SqlToken;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type InternalQueryMethod<R = any> = (
