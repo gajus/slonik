6ab872f823ecedb8463fe5f998b62bc5dc244f67
Gajus <gajus@gajus.com>
2024-12-03 13:12:45 -0600

    style: add missing line breaks

--- packages/utilities/src/utilities/parseDsn.test.ts
+++ packages/utilities/src/utilities/parseDsn.test.ts
@@ -7,40 +7,49 @@ const testParse = test.macro((t, connectionOptions: ConnectionOptions) => {
 });
 
 test('postgresql://', testParse, {});
+
 test('postgresql://localhost', testParse, {
   host: 'localhost',
 });
+
 test('postgresql://localhost:5432', testParse, {
   host: 'localhost',
   port: 5_432,
 });
+
 test('postgresql://localhost/foo', testParse, {
   databaseName: 'foo',
   host: 'localhost',
 });
+
 test('postgresql://foo@localhost', testParse, {
   host: 'localhost',
   username: 'foo',
 });
+
 test('postgresql://foo:bar@localhost', testParse, {
   host: 'localhost',
   password: 'bar',
   username: 'foo',
 });
+
 test('postgresql://localhost/?&application_name=baz', testParse, {
   applicationName: 'baz',
   host: 'localhost',
 });
+
 test('postgresql://localhost/?options=-c%20search_path%3Dfoo', testParse, {
   host: 'localhost',
   options: '-c search_path=foo',
 });
+
 test('postgresql://fo%2Fo:b%2Far@localhost/ba%2Fz', testParse, {
   databaseName: 'ba/z',
   host: 'localhost',
   password: 'b/ar',
   username: 'fo/o',
 });
+
 test(
   // cspell: disable-next-line
   'postgresql://db_user:db_password@%2Fcloudsql%2Fproject-id%3Aregion-id1%3Acloudsqlinstance-name/database-name',
